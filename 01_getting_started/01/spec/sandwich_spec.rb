7a4d7bf84ba58bfb2ed85582d8db21511eadef1b
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-12 22:14:38 +0000

    Fix linter errors

--- 01_getting_started/01/spec/sandwich_spec.rb
+++ 01_getting_started/01/spec/sandwich_spec.rb
@@ -1,20 +1,18 @@
 Sandwich = Struct.new(:taste, :toppings)
 
-RSpec.describe "An ideal sandwich" do
-	it "should be delicious" do
-		sandwich = Sandwich.new('delicious', [])
+RSpec.describe 'An ideal sandwich' do
+  it 'should be delicious' do
+    sandwich = Sandwich.new('delicious', [])
 
-		taste = sandwich.taste
+    taste = sandwich.taste
 
-		expect(taste).to eq('delicious') 
-	end
+    expect(taste).to eq('delicious')
+  end
 
-	it "lets me add more toppings" do
-		sandwich = Sandwich.new('delicious', [])
-		sandwich.toppings << 'cheese'
-		toppings = sandwich.toppings
-		expect(toppings).not_to be_empty 
-	end
-	
-		
+  it 'lets me add more toppings' do
+    sandwich = Sandwich.new('delicious', [])
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+    expect(toppings).not_to be_empty
+  end
 end
--- 01_getting_started/01/spec/sandwich_spec_with_before_hook_spec.rb
+++ 01_getting_started/01/spec/sandwich_spec_with_before_hook_spec.rb
@@ -1,23 +1,19 @@
 Sandwich = Struct.new(:taste, :toppings)
 
-RSpec.describe "An ideal sandwich" do
-	# Using RSpec hooks[before, after]
-	# Use the 'before' hook to set up a common code across
-	# Note: This technique is not actually the best.
-	# Because the hook has to run for every example in the spec, which is expensive.
-	before { @sandwich = Sandwich.new('delicious', []) }
+RSpec.describe 'An ideal sandwich' do
+  # Using RSpec hooks[before, after]
+  # Use the 'before' hook to set up a common code across
+  # Note: This technique is not actually the best.
+  # Because the hook has to run for every example in the spec, which is expensive.
+  before { @sandwich = Sandwich.new('delicious', []) }
 
-	it "tastes delicious" do
-		taste = @sandwich.taste
-		expect(taste).to eq('delicious')
-	end
+  it 'tastes delicious' do
+    taste = @sandwich.taste
+    expect(taste).to eq('delicious')
+  end
 
-	it "does not have an empty toppings" do
-		toppings = @sandwich.toppings << 'cheese'
-		expect(toppings).not_to be_empty
-	end
-	
-	
-	
+  it 'does not have an empty toppings' do
+    toppings = @sandwich.toppings << 'cheese'
+    expect(toppings).not_to be_empty
+  end
 end
-
