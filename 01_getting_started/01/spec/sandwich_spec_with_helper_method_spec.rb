2da00f6ace1308e3aa402a666ee76c394828602e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-12 22:13:01 +0000

    Use helper method for the spec setup

---
+++ 01_getting_started/01/spec/sandwich_spec_with_helper_method_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'An ideal sandwich' do
+	Sandwich = Struct.new(:taste, :toppings)
+	def sandwich
+		# Remember to memoize the variable
+		@sandwich ||= Sandwich.new('delicious', [])
+	end
+	
+	it 'is delicious' do
+	taste = sandwich.taste
+	expect(taste).to eq('delicious')
+	end
+	it 'lets me add toppings' do
+	sandwich.toppings << 'cheese'
+	toppings = sandwich.toppings
+	expect(toppings).not_to be_empty
+	end
+	end
