cb2d4d6a52ac4cbc1880006386f55ead22c3d3a2
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-12 21:40:55 +0000

    Add comments for hook usage

--- 01_getting_started/01/spec/sandwich_spec_with_before_hook_spec.rb
+++ 01_getting_started/01/spec/sandwich_spec_with_before_hook_spec.rb
@@ -3,6 +3,8 @@ Sandwich = Struct.new(:taste, :toppings)
 RSpec.describe "An ideal sandwich" do
 	# Using RSpec hooks[before, after]
 	# Use the 'before' hook to set up a common code across
+	# Note: This technique is not actually the best.
+	# Because the hook has to run for every example in the spec, which is expensive.
 	before { @sandwich = Sandwich.new('delicious', []) }
 
 	it "tastes delicious" do
