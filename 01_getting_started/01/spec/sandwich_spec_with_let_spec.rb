03ef132c1631519db3a9b608af7465faa146dde4
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-19 21:46:06 +0000

    Remove the context method

--- 01_getting_started/01/spec/sandwich_spec_with_let_spec.rb
+++ 01_getting_started/01/spec/sandwich_spec_with_let_spec.rb
@@ -1,17 +1,15 @@
 Sandwich = Struct.new(:taste, :toppings)
 
-RSpec.describe "Sanwich" do
+RSpec.describe "An ideal Sanwich" do
 	let(:sandwich) {Sandwich.new('delicious', [])}
-	context "An ideal Sanwich" do
-		it "tastes delicious" do
-			taste = sandwich.taste
-			expect(taste).to eq('delicious')
-		end
+	it "tastes delicious" do
+		taste = sandwich.taste
+		expect(taste).to eq('delicious')
+	end
 
-		it 'lets me add toppings' do
-			sandwich.toppings << 'cheese'
-			toppings = sandwich.toppings
-			expect(toppings).not_to be_empty
-		end
+	it 'lets me add toppings' do
+		sandwich.toppings << 'cheese'
+		toppings = sandwich.toppings
+		expect(toppings).not_to be_empty
 	end
 end
